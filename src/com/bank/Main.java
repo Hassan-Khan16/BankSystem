8e278d1cb294e7540f346dacce71bfd2c5815930
Hassan Khan <hassankpisces@gmail.com>
2024-09-06 02:14:24 +0500

    step 2: JLabel and JButton added and displayed

--- src/com/bank/Main.java
+++ src/com/bank/Main.java
@@ -1,14 +1,21 @@
 package com.bank;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Bank System");
-
 		frame.setSize(600,450);
-
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.setLayout(new FlowLayout());
+
+		JLabel welcomeLabel = new JLabel("Welcome to the Bank");
+		JButton demoButton = new JButton("Demo button");
+
+		frame.add(welcomeLabel);
+		frame.add(demoButton);
 
 		frame.setVisible(true);
 	}
